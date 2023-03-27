7637bc855af8f9a2080e5911914edf76e7e9132b
paveltretyak <paveltretyak87@gmail.com>
2023-03-27 11:44:56 +0300

    + slider

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    let deadline = '2023-02-10'; // Задаем дату окончания таймера
+    let deadline = '2023-10-10'; // Задаем дату окончания таймера
 
     function getTimeRemaining(endtime) {    //Функция, отсчитывающая, сколько времени осталось
         let t = Date.parse(endtime) - Date.parse(new Date());
@@ -105,3 +105,58 @@ window.addEventListener('DOMContentLoaded', function() {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';          // Разблокируем прокрутку
     });
+
+    // Слайдер
+
+    let slideIndex = 1;  // Параметр текущего слайда
+    let slides = document.querySelectorAll('.slider-item');
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    let dotsWrap = document.querySelector('.slider-dots');
+    let dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;                 
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        /*for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        } Это одно и то же*/ 
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide (n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+      plusSlides(-1);  
+    })
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    })
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+               currentSlide(i); 
+            }
+        }
+    })
